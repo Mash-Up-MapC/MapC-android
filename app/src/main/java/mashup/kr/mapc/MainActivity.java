a6783cd92e8fc1ace60b4597f78bcc82fae04950
gaon-kim <gaon.kim.dev@gmail.com>
2018-06-30 22:51:18 +0900

    bottom navigation view와 fragments 연동

--- app/src/main/java/mashup/kr/mapc/MainActivity.java
+++ app/src/main/java/mashup/kr/mapc/MainActivity.java
@@ -1,23 +1,86 @@
 package mashup.kr.mapc;
 
+import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         removeShiftMode(bottomNavigationView);
+
+        viewPager = findViewById(R.id.view_pager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+                        break;
+                    case 1:
+                        bottomNavigationView.setSelectedItemId(R.id.menu_qnp);
+                        break;
+                    case 2:
+                        bottomNavigationView.setSelectedItemId(R.id.menu_magazine);
+                        break;
+                    case 3:
+                        bottomNavigationView.setSelectedItemId(R.id.menu_mypage);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu_home:
+                                viewPager.setCurrentItem(0, true);
+                                break;
+                            case R.id.menu_qnp:
+                                viewPager.setCurrentItem(1, true);
+                                break;
+                            case R.id.menu_magazine:
+                                viewPager.setCurrentItem(2, true);
+                                break;
+                            case R.id.menu_mypage:
+                                viewPager.setCurrentItem(3, true);
+                                break;
+                        }
+
+                        return true;
+                    }
+                }
+        );
     }
 
     static void removeShiftMode(BottomNavigationView view) {
