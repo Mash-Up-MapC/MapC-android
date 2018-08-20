4bc3fd8a83e0e0b6e279b18283cb649613fab75e
gaon-kim <gaon.kim.dev@gmail.com>
2018-08-20 16:08:24 +0900

    테스트 코드 패키지명 수정 및 splash activity 패키지명 수정

--- app/src/androidTest/java/kr/mashup/mapc/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/kr/mashup/mapc/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("mashup.kr.mapc", appContext.getPackageName());
+        assertEquals("kr.mashup.mapc", appContext.getPackageName());
     }
 }
