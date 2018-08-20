50d0bb1a4d495349c617d880f3651ea5a6bc7a88
JSpiner <jspiner@naver.com>
2018-08-20 10:59:26 +0900

    splash activity 추가

---
+++ app/src/main/java/mashup/kr/mapc/ui/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package mashup.kr.mapc.ui.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import mashup.kr.mapc.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
