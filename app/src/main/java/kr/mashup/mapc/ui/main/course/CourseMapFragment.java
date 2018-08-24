e8b9d13b2e64500ba2247eec2255303562cfd501
Gaon Kim <gaon.kim.dev@gmail.com>
2018-08-25 02:49:13 +0900

    CourseMapFragment 생성

---
+++ app/src/main/java/kr/mashup/mapc/ui/main/course/CourseMapFragment.java
@@ -0,0 +1,63 @@
+package kr.mashup.mapc.ui.main.course;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import kr.mashup.mapc.R;
+
+public class CourseMapFragment extends Fragment implements OnMapReadyCallback {
+
+    private View view;
+    private MapView mapView;
+
+    public CourseMapFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_course_map, container, false);
+
+        Log.e("mapView", "mapView created.");
+        mapView = view.findViewById(R.id.course_map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(this.getActivity());
+    }
+}
