44244bd2ca6393dd3855f6143c07cd33bef54014
jspiner <jspiner@naver.com>
2018-08-30 21:22:58 +0900

    busview 이미지 리소스 추가

--- app/src/main/java/kr/mashup/mapc/ui/splash/BusView.java
+++ app/src/main/java/kr/mashup/mapc/ui/splash/BusView.java
@@ -1,12 +1,22 @@
 package kr.mashup.mapc.ui.splash;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import kr.mashup.mapc.R;
+
 public class BusView extends View {
+
+    private Bitmap imageBackground;
+    private Bitmap imageCloud;
+    private Bitmap imageBusTop;
+    private Bitmap imageBusBottom;
 
     public BusView(Context context) {
         super(context);
@@ -21,7 +31,18 @@ public class BusView extends View {
     }
 
     private void init() {
-        
+        loadResource();
+    }
+
+    private void loadResource() {
+        imageBackground = decodeResource(R.drawable.background_splash_bus);
+        imageCloud = decodeResource(R.drawable.img_splash_cloud);
+        imageBusTop = decodeResource(R.drawable.img_splash_bus_top);
+        imageBusBottom = decodeResource(R.drawable.img_splash_bus_bottom);
+    }
+
+    private Bitmap decodeResource(@DrawableRes int drawableId) {
+        return BitmapFactory.decodeResource(getResources(), drawableId);
     }
 
     @Override
