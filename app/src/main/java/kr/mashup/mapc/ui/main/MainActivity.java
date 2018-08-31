38046d867d2b6c8bd002bdf0bdf632f778d6836d
jspiner <jspiner@naver.com>
2018-08-31 23:40:01 +0900

    커스텀 뷰로 탭에 아이콘/텍스트 추가

--- app/src/main/java/kr/mashup/mapc/ui/main/MainActivity.java
+++ app/src/main/java/kr/mashup/mapc/ui/main/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import kr.mashup.mapc.R;
 
@@ -12,20 +16,38 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private static final int PAGER_SCREEN_OFFSET_LIMIT = 3;
 
+    private static final int[] TAB_IMAGES = {R.drawable.ic_course, R.drawable.ic_booking, R.drawable.ic_guide, R.drawable.ic_bus, R.drawable.ic_etc};
+    private static final String[] TAB_TEXTS = {"코스", "예약", "가이드", "버스", "전체"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TabLayout tabLayout = findViewById(R.id.tab_layout);
 
         viewPager = findViewById(R.id.view_pager);
         viewPager.setOffscreenPageLimit(PAGER_SCREEN_OFFSET_LIMIT);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
 
-        tabLayout.setupWithViewPager(viewPager);
+        initTab();
 
         getSupportActionBar().setElevation(0);
+    }
+
+    private void initTab() {
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            View tabView = LayoutInflater.from(this).inflate(R.layout.item_main_tab, null, false);
+
+            ImageView tabIcon = tabView.findViewById(R.id.tab_icon);
+            TextView tabText = tabView.findViewById(R.id.tab_text);
+
+            tabIcon.setImageResource(TAB_IMAGES[i]);
+            tabText.setText(TAB_TEXTS[i]);
+            tabLayout.getTabAt(i).setCustomView(tabView);
+        }
     }
 }
