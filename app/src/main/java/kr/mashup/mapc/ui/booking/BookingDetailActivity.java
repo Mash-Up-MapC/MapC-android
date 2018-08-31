9db5d53b128e5118a37b6bdb68f99930a8e98d25
jspiner <jspiner@naver.com>
2018-09-01 00:18:11 +0900

    예약 상세 페이지 추가

---
+++ app/src/main/java/kr/mashup/mapc/ui/booking/BookingDetailActivity.java
@@ -0,0 +1,16 @@
+package kr.mashup.mapc.ui.booking;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import kr.mashup.mapc.R;
+
+public class BookingDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_detail);
+    }
+}
