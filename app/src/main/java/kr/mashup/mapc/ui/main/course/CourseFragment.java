d3da960168b59cc30bbcc5aff508b6f0b526028f
Gaon Kim <gaon.kim.dev@gmail.com>
2018-09-02 18:38:45 +0900

    코스 메인 페이지 레이아웃 작업
    이미지 교체 필요

--- app/src/main/java/kr/mashup/mapc/ui/main/course/CourseFragment.java
+++ app/src/main/java/kr/mashup/mapc/ui/main/course/CourseFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import kr.mashup.mapc.R;
+import kr.mashup.mapc.ui.main.data.Course;
 
 public class CourseFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private CourseAdapter adapter;
 
     @Nullable
     @Override
@@ -21,6 +27,23 @@ public class CourseFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+    }
+
+    private void init(View view) {
+        view.findViewById(R.id.recycler_view_course).setFocusable(false);
+        view.findViewById(R.id.main_image).requestFocus();
+
+        recyclerView = view.findViewById(R.id.recycler_view_course);
+
+        adapter = new CourseAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+        for (int i = 0; i < 10; i++) {
+            adapter.addData(new Course());
+        }
     }
 
     public static CourseFragment newInstance() {
