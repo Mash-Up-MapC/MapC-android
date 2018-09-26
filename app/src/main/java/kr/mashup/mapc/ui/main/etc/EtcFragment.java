22b27bfb121f00ff7539b11744edce64d0b216a8
jspiner <jspiner@naver.com>
2018-09-26 17:16:25 +0900

    전체페이지 리스트 추가

--- app/src/main/java/kr/mashup/mapc/ui/main/etc/EtcFragment.java
+++ app/src/main/java/kr/mashup/mapc/ui/main/etc/EtcFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,13 @@ import android.view.ViewGroup;
 import kr.mashup.mapc.R;
 
 public class EtcFragment extends Fragment {
+
+    private static final String[] items = {
+            "언어설정",
+            "공지사항",
+            "이벤트",
+            "고객센터"
+    };
 
     @Nullable
     @Override
@@ -21,6 +30,13 @@ public class EtcFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        EtcAdapter adapter = new EtcAdapter();
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+        adapter.addDatas(items);
     }
 
     public static EtcFragment newInstance() {
