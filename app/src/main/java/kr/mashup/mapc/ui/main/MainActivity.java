3c52800530f8594bdcf6ecc75fe53318adf7b5bd
jspiner <jspiner@naver.com>
2018-09-26 17:19:41 +0900

    페이지가 바뀔때, 기타페이지에서는 status bar를 붉은색으로 자연스럽게 변경

--- app/src/main/java/kr/mashup/mapc/ui/main/MainActivity.java
+++ app/src/main/java/kr/mashup/mapc/ui/main/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends BaseActivity {
 
     private static final int[] TAB_IMAGES = {R.drawable.ic_course, R.drawable.ic_booking, R.drawable.ic_guide, R.drawable.ic_bus, R.drawable.ic_etc};
     private static final String[] TAB_TEXTS = {"코스", "예약", "가이드", "버스", "전체"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +32,34 @@ public class MainActivity extends BaseActivity {
         viewPager.setOffscreenPageLimit(PAGER_SCREEN_OFFSET_LIMIT);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                        setStatusBarColor(Color.WHITE);
+                        break;
+                    case 4:
+                        setStatusBarColor(getColor(R.color.salmon));
+                        break;
+                    default:
+                        throw new RuntimeException("예상치 못한 position : " + position);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         initTab();
     }
